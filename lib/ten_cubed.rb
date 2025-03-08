7682dffee50ade687e30c829dc1efdf219faaa1c
Seb <sebastian@lemery.io>
2025-03-08 13:56:01 -0600

    Cleanup

--- lib/ten_cubed.rb
+++ lib/ten_cubed.rb
@@ -2,6 +2,10 @@
 
 require "active_record"
 require_relative "ten_cubed/version"
+require_relative "ten_cubed/configuration"
+require_relative "ten_cubed/connection"
+require_relative "ten_cubed/models/concerns/ten_cubed_user"
+require_relative "ten_cubed/engine" if defined?(Rails)
 
 module TenCubed
   class Error < StandardError; end
@@ -17,8 +21,3 @@ module TenCubed
     end
   end
 end
-
-require_relative "ten_cubed/configuration"
-require_relative "ten_cubed/connection"
-require_relative "ten_cubed/models/concerns/ten_cubed_user"
-require_relative "ten_cubed/engine" if defined?(Rails)
